07f5faf930e99ec9fd1c06457300f578576df051
comolokko <samederturk55@gmail.com>
2023-06-19 15:38:29 +0300

    proje tamamlandi

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const searchInput = document.getElementById("searchValue");
 const searchClick = document.getElementById("search");
 const API = "https://api.giphy.com/v1/gifs/search?api_key=eNZlPkO49tfPCTfOc4Kp25wFfJIhidnB&q=";
-const apiDevam = "&limit=10&offset=0&rating=g&lang=en&bundle=messaging_non_clips";
+const apiDevam = "&limit=20&offset=0&rating=g&lang=en&bundle=messaging_non_clips";
 
 
 
@@ -9,5 +9,26 @@ searchClick.addEventListener("click", async() => {
     const fullAPI = API + searchInput.value + apiDevam;
     const request = await fetch(fullAPI);
     const response = await request.json();
-    const data = console.log(response);
+    clearUI();
+    showGiphy(response.data);
 });
+
+const showUI = document.getElementById("showGiphy");
+
+const showGiphy = data => {
+    data.forEach(function(element) {
+        const tag = `
+       <div class="col-md-3">
+            <div class="card">
+                <img src="${element.images.original.url}" alt="${element.title}" class="card-img-top">
+            </div>
+        </div>
+       `;
+       showUI.insertAdjacentHTML("beforeend", tag);
+    });
+};
+
+const clearUI = ()=> {
+    showUI.innerHTML = "";
+}
+
